c0de520c8aff65bbc7b057966ba7045acac8c520
Cagri Esen <esencgr@gmail.com>
2019-01-04 14:36:37 +0000

    add file

--- array_element_average.cpp
+++ array_element_average.cpp
@@ -6,11 +6,11 @@
 using namespace std;
 int main(){
     srand(time(NULL));
-    
+
     int size;
     cout << "enter the size of array = ";
     cin >> size;
-    
+
     cout << "array = ";
     int a[size];
         for (int i=0; i<=size-1; i++){
@@ -18,16 +18,16 @@ int main(){
             cout << a[i] << " ";
         }
     cout << endl;
-        
+
     double sum=0, product=1;
         for (int i=0; i<=size-1; i++){
              sum += a[i];
              product *= a[i];
         }
-           
+
     cout << "sum of array's elements = " << sum << endl;
-    cout << "average of array's elements = " << double(sum / size)<< endl;  
+    cout << "average of array's elements = " << double(sum / size)<< endl;
     cout << "geometric average of array's elements = " << pow(product,(double)1/size) << endl;
-    
+
 return 0;
 }
