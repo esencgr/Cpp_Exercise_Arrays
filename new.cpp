e047ed0ced173aa1fcd307cccc50240ae1bf5f48
Cagri Esen <esencgr@gmail.com>
2019-03-30 14:31:12 +0300

    modified

--- new.cpp
+++ new.cpp
@@ -9,17 +9,19 @@ int main(){
   srand(time(0));
   int n [SIZE], sum = 0;
   cout << "input array : ";
-       for (int i = 0; i <= SIZE - 1; i++){
+      for (int i = 0; i <= SIZE - 1; i++){
            n[i] =  1 + rand () % 6;
            cout << n[i] << " ";
            sum += n[i];
        }
   cout << endl;
+
   int min = n[0];
       for (int i = 0; i <= SIZE - 1; i++){
           if (min > n[i])
              min = n[i];
       }
+
    cout << "minimum value = " << min << endl;
    cout << "sum of all = " << sum << endl;
    cout << "sum of greatest 4 value = " << sum - min << endl;
