21850c84f204017a5acd5d950bc10b9a0581e3cf
Cagri Esen <esencgr@gmail.com>
2019-03-30 14:16:00 +0300

    add file

--- new.cpp
+++ new.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
+#include <math.h>
+#include <time.h>
+#include <stdlib.h>
+#define SIZE 5
 using namespace std;
-int main() {
-  cout << "hello world" << endl;
-  int number;
-  cin >> number;
-
-      if (number == 5)
-          cout << "number = 5 " << endl;
-      else
-          cout << "number =! 5" << endl;
-
-  cout << "işlem devam etsin mi (y=1)?" << endl;
-  int num;
-  cin >> num;
-  if (num == 1) {
-      cout << "döngüye giriliyor"<< endl;
-
-      for (int i=0; i<10; i++){
-            if (i%4)
-              cout << i << " ";
-            else
-              cout << endl;
+int main(){
+  
+  srand(time(0));
+  int n [SIZE], sum = 0;
+  cout << "input array : ";
+       for (int i = 0; i <= SIZE - 1; i++){
+           n[i] =  1 + rand () % 6;
+           cout << n[i] << " ";
+           sum += n[i];
+       }
+  cout << endl;
+  int min = n[0];
+      for (int i = 0; i <= SIZE - 1; i++){
+          if (min > n[i])
+             min = n[i];
       }
-   }
+   cout << "minimum value = " << min << endl;
+   cout << "sum of all = " << sum << endl;
+   cout << "sum of greatest 4 value = " << sum - min << endl;
 }
